d37cde49e9a71cf87a5058e581b74e45a4cebcf1
nsnell34 <126209391+nsnell34@users.noreply.github.com>
2024-02-26 18:39:28 -0600

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+error_reporting(-1);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL ^ E_WARNING ^ E_DEPRECATED);
+ini_set("session.gc_maxlifetime", "180000");
+
+ignore_user_abort(1);
+set_time_limit(1800);
+
+
+class GUI {
+
+    public function display(){
+            $x = 2;
+    }
+}
+
+
